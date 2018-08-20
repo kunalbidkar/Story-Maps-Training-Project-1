719fe93ed63f63a2a009f2b6994b52008070f340
Kunal Bidkar <kunalbidkar13@gmail.com>
2018-08-20 12:27:05 -0400

    changing the paths
    changed the paths of images based on the parent folder of js and images

--- build/js/dataFile.js
+++ build/js/dataFile.js
@@ -5,7 +5,7 @@ var data = [
         Description:
             "North Bridge was built in 1876 which was made up of steel and aluminium. This bridge still maintains its beauty and is used mostly for biking and walking.",
         Location: [19.82, 41.33],
-        ImageURL: "./images/optimized_image1.jpg"
+        ImageURL: "../images/optimized_image1.jpg"
     },
     {
         ID: 2,
@@ -13,7 +13,7 @@ var data = [
         Description:
             "West Bridge was built in 1932 which was made up mostly with wood and some part with steel. This bridge still maintains its beauty and is used mostly for biking and walking and doing regular exercises.",
         Location: [16.37, 48.21],
-        ImageURL: "./images/optimized_image2.jpg"
+        ImageURL: "../images/optimized_image2.jpg"
     },
     {
         ID: 3,
@@ -21,7 +21,7 @@ var data = [
         Description:
             " East Bridge was built in 1823 and is the oldest of all the bridges. This was made up of steel, wood, and aluminium. This bridge is used mostly for transportation by cars and automobiles and is avoided to be used by bikers and pedestrians.",
         Location: [18.38, 43.85],
-        ImageURL: "./images/optimized_image3.jpg"
+        ImageURL: "../images/optimized_image3.jpg"
     },
     {
         ID: 4,
@@ -29,6 +29,6 @@ var data = [
         Description:
             "South Bridge was built in 1910, made up of steel and aluminium. This bridge still maintains its beauty and is used mostly for biking and walking purposes. This was made by 48 workers united to solve the problem of passing the river.",
         Location: [23.32, 42.7],
-        ImageURL: "./images/optimized_image4.jpg"
+        ImageURL: "../images/optimized_image4.jpg"
     }
 ];
